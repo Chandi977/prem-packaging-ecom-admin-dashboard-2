67d9488974d8039271335a198b34da39642e9897
Chandi Charan Mahato <55855799+Chandi977@users.noreply.github.com>
2025-12-17 17:34:33 +0530

    Update DEV API endpoint to production URL

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,5 +1,6 @@
 // export const DEV = "http://15.207.107.52:3000/api";
 // export const DEV = "https://api-dev.assertit.io /premind/api/"
 // export const DEV = "https://server.prempackaging.com/premind/api";
-export const DEV = "http://localhost:5000/premind/api";
+// export const DEV = "http://localhost:5000/premind/api";
+export const DEV = "https://prem-packaging-ecom-backend-2.onrender.com/premind/api";
 // export const DEV = "https://server.prempackaging.com/premind/api/";
